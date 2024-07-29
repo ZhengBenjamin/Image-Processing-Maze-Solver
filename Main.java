5ff77ab937162c9a3fc99cf5d87e6f8c62b0c912
Benjamin Zheng <54424912+ZhengBenjamin@users.noreply.github.com>
2024-07-28 21:11:33 -0400

    CREATE Main.java

---
+++ Main.java
@@ -0,0 +1,22 @@
+import java.awt.image.*;
+import java.io.*;
+import javax.imageio.ImageIO;
+
+public class Main {
+  
+  private static UI ui;
+
+  public static void main(String[] args) {
+    File file = new File("maze.jpg");
+    BufferedImage img = null;
+
+    try {
+      img = ImageIO.read(file);
+    } catch (IOException e) {
+      System.out.println("Error: " + e);
+    }
+
+    ui = new UI();
+  }
+
+}
